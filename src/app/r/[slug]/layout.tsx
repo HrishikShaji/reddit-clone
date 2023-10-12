c398aa8866c4db812bd702682c0a24e7700f3952
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-12 12:52:00 +0530

    editor started

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 import { format } from "date-fns";
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/Button";
 
 const Layout = async ({
   children,
@@ -54,11 +56,11 @@ const Layout = async ({
       <div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+          <div className=" overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4 ">About r/{subreddit.name}</div>
           </div>
           <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-sm bg-white">
-            <div className="flex ijustify-between gap-x-4 py-3">
+            <div className="flex justify-between gap-x-4 py-3">
               <dt className="text-gray-700">Created</dt>
               <dd className="text-gray-700">
                 <time dateTime={subreddit.createdAt.toDateString()}>
@@ -84,6 +86,15 @@ const Layout = async ({
                 isSubscribed={isSubscribed}
               />
             ) : null}
+            <Link
+              className={buttonVariants({
+                variant: "outline",
+                className: "w-full mb-6",
+              })}
+              href={`r/${slug}/submit`}
+            >
+              Create Post
+            </Link>
           </dl>
         </div>
       </div>
