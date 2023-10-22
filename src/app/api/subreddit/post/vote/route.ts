a8f7b0b6479b49853cfa0b6fd61c10a7dbf65327
HrishikShaji <hrishikminishaji@gmail.com>
2023-10-22 22:17:11 +0530

    need to sleep

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,4 +1,5 @@
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { PostVoteValidator } from "@/lib/validators/vote";
 
 export async function PATCH(req: Request) {
@@ -9,8 +10,50 @@ export async function PATCH(req: Request) {
 
     const session = await getAuthSession();
 
-    if (!session?.user) {
+    if (!session?.user?.id) {
       return new Response("Unauthorized");
+    }
+
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    const post = await db.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+
+    if (!post) {
+      return new Response("Post not found");
+    }
+
+    if (existingVote) {
+      if (existingVote.type === voteType) {
+        await db.vote.delete({
+          where: {
+            userId: session.user.id,
+            postId: postId,
+          },
+        });
+        return new Response("OK");
+      }
+      await db.vote.update({
+        where: {
+          postId,
+          userId: session.user.id,
+        },
+        data: {
+          type: voteType,
+        },
+      });
     }
   } catch (error) {}
 }
